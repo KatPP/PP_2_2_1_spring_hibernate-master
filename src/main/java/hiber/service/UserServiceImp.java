67853adafdc52f1da0e7f5c03e6fda539f293d4b
zenit1925 <kostia-85-85@yandex.ru>
2022-12-26 17:24:24 +0300

    Task 2.2.1 version3 26/12/2022

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -2,14 +2,19 @@ package hiber.service;
 
 import hiber.dao.UserDao;
 import hiber.model.User;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Service
 public class UserServiceImp implements UserService {
+
+   @Autowired
+   private SessionFactory sessionFactory;
 
    @Autowired
    private UserDao userDao;
@@ -29,7 +34,9 @@ public class UserServiceImp implements UserService {
    @Transactional(readOnly = true)
    @Override
    public List<User> list() {
-      return userDao.list();
+      String hql = "FROM User u " + "WHERE u.car.model ='Ford' and u.car.series = 1";
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery(hql);
+      return query.getResultList();
    }
 
 }
